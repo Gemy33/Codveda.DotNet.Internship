f53e7484003b8127015e1643ab543ea7432f2b72
Gemy33 <mohamedgamalabdelhaliym@gmail.com>
2026-03-14 21:16:09 +0200

    implement 3-tier layer [models - service - programe] setup the dependanceis and end from models project

---
+++ StudentManagement.Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+   
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public Person(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+
+            public virtual void DisplayInfo()
+            {
+                Console.WriteLine($"ID: {Id}, Name: {Name}");
+            }
+        
+    }
+}
---
+++ StudentManagement.Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public class Student : Person
+    {
+        public string Course { get; set; }
+
+        public Student(int id, string name, string course)
+            : base(id, name)
+        {
+            Course = course;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {Id}, Name: {Name}, Course: {Course}");
+        }
+    }
+}
