7d5d68e6fd6e87024b3c4a9363ea287586682724
Gemy33 <mohamedgamalabdelhaliym@gmail.com>
2026-03-14 21:13:12 +0200

    Add project files.

---
+++ Level1-CSharp-OOP-StudentManagement-Codvead/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Level1_CSharp_OOP_StudentManagement_Codvead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
